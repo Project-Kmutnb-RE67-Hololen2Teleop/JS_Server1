f11524dcb4ad30fc271dc920982ecb4921ca6d48
Lighthit <poomipatsubinmit@gmail.com>
2024-08-15 18:52:10 +0700

    -

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { config } from 'dotenv'
+import Log from "./utils/function/Logging_msg.js"
 import axios from 'axios';
 config()
 let central = express()
@@ -8,15 +9,16 @@ let PORT = process.env.PORT || 11233
 
 central.get('/', (req , res) => {
     res.send('Hello World!')
-    console.log(`GET Status from path "/": ${res.statusCode}`);
+    Log(`GET Status from path "/": ${res.statusCode}`);
   });
 
 central.post("/api/upload",(req , res) =>{
     let recievingData = req.body
     res.send(`${res.statusCode}`)
     
-    console.log(`Recieved http://${IP}:${PORT}/api/upload ${res.statusCode} `)
+    Log(`Recieved http://${IP}:${PORT}/api/upload ${res.statusCode} `)
   })
 central.listen(PORT, IP , ()=>{
-    console.log(`connecting to http://${IP}:${PORT}`)
+    Log(`connecting to http://${IP}:${PORT}`)
+
 })
---
+++ utils/function/Logging_msg.js
@@ -0,0 +1,5 @@
+function Log(message) {
+    console.log(message);
+}
+
+export default Log ; 
