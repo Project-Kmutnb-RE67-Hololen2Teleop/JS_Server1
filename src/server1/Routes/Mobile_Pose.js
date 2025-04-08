343f6fb974d51911d7fb3984c2c85450924d1147
Lighthit <poomipatsubinmit@gmail.com>
2025-04-08 13:41:14 +0700

    add mobile_trigger

--- src/server1/Routes/Mobile_Pose.js
+++ src/server1/Routes/Mobile_Pose.js
@@ -1,3 +1,5 @@
+import fastify from "fastify";
+
 let Pose = {
     "x":0.0,
     "y":0.0,
@@ -6,9 +8,14 @@ let Pose = {
     "yaw":0.0
 }
 
-
+let CMD_POSE = {
+    "CMD_X":0.0,
+    "CMD_Y":0.0,
+    "STATUS":false
+}
 
 export default function MobilePose(fastify,options,done){
+    // MARK:   HOLOLENS MOBILE ROBOT MOVEMENT 
 
     fastify.post("/MyAGV/Position/update",async (request,reply)=>{
        let {x , y , row , pitch , yaw } = request.body;
@@ -19,6 +26,32 @@ export default function MobilePose(fastify,options,done){
     fastify.get("/MyAGV/Position/current",async (request,reply)=>{
         console.log("send current mobile location successed")
         return reply.send(Pose)
+    });
+    // -------------------------------
+    // MARK:   COMMAND CONTROL NAVIGATION
+    fastify.post("/MyAGV/Position/control",async (request,reply)=>{
+        let {CMD_X , CMD_Y , STATUS } = request.body;
+        CMD_POSE.CMD_X = CMD_X ; CMD_POSE.CMD_Y = CMD_Y ; CMD_POSE.STATUS = STATUS ;
+        console.log(" upload control ");
+        return reply.send(reply.statusCode)
+    })
+    fastify.get("/MyAGV/Position/control/setup",async (request,reply)=>{
+        console.log("send current mobile location successed")
+        return reply.send({
+            "CMD_X":0.0,
+            "CMD_Y":0.0
+        })
+    });
+    //-----------------------------------
+    // MARK:   Trigger NAV CTRL
+    fastify.post('/MyAGV/status/update',async (request,reply)=>{
+        let { STATUS } = request.body ; 
+        CMD_POSE.STATUS = STATUS ;
+        console.log(`trigger status code : ${reply.statusCode}`);
+    });
+    
+    fastify.get('/MyAGV/status/current',async (request,reply)=>{
+        return reply.send({"STATUS":CMD_POSE.STATUS});
     });
     done();
 }
