36676c4ab55a9ecfeb4d963745cf468c61cbc402
Lighthit <poomipatsubinmit@gmail.com>
2025-06-22 14:22:14 +0700

    adding Hololens2 Gesture

--- src/server1/Routes/Router.js
+++ src/server1/Routes/Router.js
@@ -3,10 +3,13 @@ import MobilePose from './Mobile_Pose.js';
 import ControlJoint from './Manipulator.js';
 import UPLOAD_FILES from './FIlesHandler.js';
 import Location_Object from './ObjectLocation.js';
+import HololensGestures from './HololensGesture.js';
 export default function Routes_Registered(fastify){
     fastify.register(SpeedRouter);
     fastify.register(ControlJoint);
     fastify.register(UPLOAD_FILES);
     fastify.register(Location_Object);
     fastify.register(MobilePose);
+    fastify.register(HololensGestures);
+
 }
