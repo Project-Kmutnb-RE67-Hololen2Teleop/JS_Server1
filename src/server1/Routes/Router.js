4e7b415bf4a42a82ef608baddeff1fbd61cd4179
Lighthit <poomipatsubinmit@gmail.com>
2026-01-03 21:35:47 +0700

    remove all unused

--- src/server1/Routes/Router.js
+++ src/server1/Routes/Router.js
@@ -1,16 +1,11 @@
-import SpeedRouter from './speed.js';
-import MobilePose from './Mobile_Pose.js';
 import ControlJoint from './Manipulator.js';
-import UPLOAD_FILES from './FIlesHandler.js';
+
 import Location_Object from './ObjectLocation.js';
 import HololensGestures from './HololensGesture.js';
 import objectTracking from './objectTracking.js';
 export default function Routes_Registered(fastify){
-    fastify.register(SpeedRouter);
     fastify.register(ControlJoint);
-    fastify.register(UPLOAD_FILES);
     fastify.register(Location_Object);
-    fastify.register(MobilePose);
     fastify.register(HololensGestures);
     fastify.register(objectTracking);
 }
