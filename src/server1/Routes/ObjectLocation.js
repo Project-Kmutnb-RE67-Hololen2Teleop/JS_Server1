c377567638e4645786af30f7ba4d036de018f404
Lighthit <poomipatsubinmit@gmail.com>
2026-01-24 11:21:57 +0700

    update  routes

--- src/server1/Routes/ObjectLocation.js
+++ src/server1/Routes/ObjectLocation.js
@@ -1,16 +1,16 @@
 
 
 let VIRTUAL_FRAME_LOCATION_OBJ = {
-    "aruco_1":[240,118,15,0,0,0],    //DEFAULT TESTING DATA
-    "aruco_2":[240,105,15,0,0,0],   //DEFAULT TESTING DATA
+    "aruco_0":[240,118,15,0,0,0],    //DEFAULT TESTING DATA
+    "aruco_1":[240,105,15,0,0,0],   //DEFAULT TESTING DATA
     "aruco_3":[240,112,15,0,0,0]   //DEFAULT TESTING DATA
 } 
 
 export default function Location_Object(fasify,option,done){
     fasify.post("/object/update",async (request,reply)=>{
-        let { aruco_1 , aruco_2, aruco_3 } = request.body;
+        let { aruco_1 , aruco_0, aruco_3 } = request.body;
         VIRTUAL_FRAME_LOCATION_OBJ.aruco_1 = aruco_1 ;
-        VIRTUAL_FRAME_LOCATION_OBJ.aruco_2 = aruco_2 ;
+        VIRTUAL_FRAME_LOCATION_OBJ.aruco_0 = aruco_0 ;
         VIRTUAL_FRAME_LOCATION_OBJ.aruco_3 = aruco_3 ;
         console.log("object location Updated")
         return reply.send(reply.statusCode)
